5941c3ffb5a845637a9365aa3ef53ba9ab0eb5ce
angelLujan <angel.lujan@mixen.mx>
2016-01-18 12:43:30 -0700

    Test #1

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-		window.location.href = 'http://mixen.mx/web/dexe-app';
+		alert('HOLA MUNDO');
+		window.location.href = 'http://www.mixen.mx/web/dexe-app';
     }
 };
 
