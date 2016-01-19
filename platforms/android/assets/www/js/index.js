266fe5a36244b96577b259ea6630864d80b67265
angelLujan <angel.lujan@mixen.mx>
2016-01-18 17:39:28 -0700

    new test

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -16,34 +16,27 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var cordovaLoaded = false;
+var photos = [];
+var orderID = null;
+
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    onDeviceReady: function () {
+        var ref = window.open('http://mixen.mx/web/dexe-app', '_blank', 'location=no', 'zoom=no');
     }
 };
